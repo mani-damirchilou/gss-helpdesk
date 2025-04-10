d9b7ee547f149d2b5bd35641d96b5a14b48ad37a
mani-damirchilou <manidamirchilou@gmail.com>
2025-04-10 04:58:54 -0700

    Create LimitLoginAttempts pipe class

---
+++ app/Pipes/LimitLoginAttempts.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Pipes;
+
+use App\Dto\LoginDto;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
+
+class LimitLoginAttempts
+{
+    protected int $maxAttempts = 5;
+    public function __invoke(LoginDto $loginDto,$next)
+    {
+        if (RateLimiter::tooManyAttempts($loginDto->throttleKey,$this->maxAttempts))
+        {
+            throw ValidationException::withMessages([
+                'throttle' => __('auth.throttle', [
+                    'seconds' => RateLimiter::availableIn($loginDto->throttleKey)
+                ])
+            ]);
+        }
+        return $next($loginDto);
+    }
+}
