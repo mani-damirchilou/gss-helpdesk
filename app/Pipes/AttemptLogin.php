9202d16163f434173896bde94e7104171bc1b1df
mani-damirchilou <manidamirchilou@gmail.com>
2025-04-10 04:59:42 -0700

    Create AttemptLogin pipe class

---
+++ app/Pipes/AttemptLogin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Pipes;
+
+use App\Dto\LoginDto;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
+
+class AttemptLogin
+{
+    protected int $decaySeconds = 120;
+    public function __invoke(LoginDto $loginDto,$next)
+    {
+        $credentials = [
+            filter_var($loginDto->login,FILTER_VALIDATE_EMAIL) ? 'email' : 'username' => $loginDto->login,
+            'password' => $loginDto->password
+        ];
+        $attempt = Auth::attempt($credentials,$loginDto->remember);
+        if (!$attempt)
+        {
+            RateLimiter::hit($loginDto->throttleKey,$this->decaySeconds);
+            throw ValidationException::withMessages([
+                'failed' => __('auth.failed')
+            ]);
+        }
+        RateLimiter::clear($loginDto->throttleKey);
+        session()->regenerate();
+        return $next($loginDto);
+    }
+}
