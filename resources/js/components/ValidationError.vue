2a3542110f0420880f3be2b938d5bef2951c13c9
mani-damirchilou <manidamirchilou@gmail.com>
2025-04-10 09:18:02 -0700

    Create ValidationError component

---
+++ resources/js/components/ValidationError.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+    message?: string
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+<small v-if="message" class="text-xs text-red-600 dark:text-red-500">{{ message }}</small>
+</template>
+
+<style scoped>
+
+</style>
